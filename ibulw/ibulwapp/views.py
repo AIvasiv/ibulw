aba1cb5f206c097c8af1dda1309a3961e81cf8bd
Ivasiv <ivasiv.andriy@gmail.com>
2020-12-10 21:04:06 +0200

    added a login/logout functionality with related pages

--- ibulw/ibulwapp/views.py
+++ ibulw/ibulwapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+@login_required(login_url='/tracker/sign-in/')
+def tracker_home(request):
+    return render(request, 'tracker/tracker_home.html')
+
